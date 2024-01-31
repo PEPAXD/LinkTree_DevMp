78e905beeebf7248afbc50d2aef77f5242f63f53
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-01-31 03:39:24 -0300

    firstCommit

---
+++ LinkTree/src/components/LinkTreeCard.jsx
@@ -0,0 +1,13 @@
+import './styles/LinkTreeCard.scss';
+
+function App() {
+  return (
+    <>
+      <div className="Container">
+        <h1>First Commit!!!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
