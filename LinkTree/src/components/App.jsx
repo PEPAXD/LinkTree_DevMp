6bfbf8e217b29b77c8c592773d825173869a07c5
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-02-01 03:43:20 -0300

    Add techWallpaper

---
+++ LinkTree/src/components/App.jsx
@@ -0,0 +1,15 @@
+import "./styles/LinkTreeCard.scss";
+
+function App() {
+  return (
+    <>
+      <div className="shadowWallpaper">
+        <div className="Container">
+          <h1>First Commit!!!</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
