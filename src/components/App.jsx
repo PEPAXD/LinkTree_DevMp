e56d349225131a579afbce0b67598e7c0c0c38c4
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-02-04 16:31:01 -0300

    cleanCode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,24 @@
 import React, { useState } from "react";
 import "./styles/App.scss";
-
 import LinkButton from "./LinkButton";
 import SocialButton from "./SocialButton";
-
 import devImg from "../assets/devImg.png";
 
+const linksButtons = [
+  { text: "Go to my website", href: "https://www.mauropepacvwebsite.com" },
+  { text: "My Work Portfolio", href: "https://github.com/PEPAXD?tab=stars" },
+  { text: "Download CV", onClick: () => window.open("/CV PEPA.pdf", "_blank") },
+];
+
 const socialNetworksData = [
-  { icon: "linkedin", text: "mauro-pepa-dev", color: "#0077B5"},
+  { icon: "linkedin", text: "mauro-pepa-dev", color: "#0077B5" },
   { icon: "instagram", text: "@mauropepa97", color: "#C13584" },
   { icon: "github", text: "PEPAXD", color: "#333" },
   { icon: "whatsapp", text: "ARG +549 3541-628524", color: "#25D366" },
   { icon: "mail", text: "mauropepa1997oficial@gmail.com", color: "#D44638" },
 ];
 
-const linkA = "https://www.mauropepacvwebsite.com";
-const linkB = "https://github.com/PEPAXD?tab=stars";
-
 function App() {
-
-  //View PDF
-  function viewPDF() {
-    console.log("View PDF");
-    const urlPDF = '/CV PEPA.pdf';
-    window.open(urlPDF, '_blank');
-}
-
-  // Print-SocialNetworks-Name
   const [socialNetwork, setSocialNetwork] = useState("CONTACT ME!");
   const [color, setColor] = useState("#000");
 
@@ -41,42 +33,40 @@ function App() {
   };
 
   return (
-    <>
-      <div className="shadowWallpaper">
-        <div className="container">
-          <div className="card">
-            <div className="devData">
-              <img src={devImg} alt="MyPhoto" className="DevImg" />
-              <h1>Mauro Exequiel Pepa</h1>
-              <p>Frontend Developer - Digital Designer</p>
-            </div>
+    <div className="shadowWallpaper">
+      <div className="container">
+        <div className="card">
+          <div className="devData">
+            <img src={devImg} alt="MyPhoto" className="DevImg" />
+            <h1>Mauro Exequiel Pepa</h1>
+            <p>Frontend Developer - Digital Designer</p>
+          </div>
 
-            <div className="containersLinks">
-              <LinkButton text={"Go to my website"} href={linkA}/>
-              <LinkButton text={"My Work Portfolio"} href={linkB} />
-              <LinkButton text={"Download CV"} onClick={viewPDF} />
-            </div>
+          <div className="containersLinks">
+            {linksButtons.map((button, index) => (
+              <LinkButton key={index} {...button} />
+            ))}
+          </div>
 
-            <div className="socialNetworks">
-              <h3 style={{ color: color }}>{socialNetwork}</h3>
-              <div className="bottomLine"></div>
-              <div className="containerSocialButtons">
-                {socialNetworksData.map((network) => (
-                  <SocialButton
-                    key={network.icon}
-                    icon={network.icon}
-                    onMouseOver={() =>
-                      handleMouseOver(network.text, network.color)
-                    }
-                    onMouseOut={handleMouseOut}
-                  />
-                ))}
-              </div>
+          <div className="socialNetworks">
+            <h3 style={{ color: color }}>{socialNetwork}</h3>
+            <div className="bottomLine"></div>
+            <div className="containerSocialButtons">
+              {socialNetworksData.map((network) => (
+                <SocialButton
+                  key={network.icon}
+                  icon={network.icon}
+                  onMouseOver={() =>
+                    handleMouseOver(network.text, network.color)
+                  }
+                  onMouseOut={handleMouseOut}
+                />
+              ))}
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
